6b94d1e3f226badf90717e73a69c79a0d9680a14
xupengfey <xupengfey@163.com>
2013-06-06 17:17:26 +0800

    ccc

--- include/cjson/fpconv.h
+++ include/cjson/fpconv.h
@@ -12,7 +12,8 @@ static inline void fpconv_init()
     /* Do nothing - not required */
 }
 #else
-extern inline void fpconv_init();
+//extern inline void fpconv_init();
+extern void fpconv_init();
 #endif
 
 extern int fpconv_g_fmt(char*, double, int);
